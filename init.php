f7574bd59ee6f628553ffbaaeb796fb74cc18607
MoeChanMyaeHtun <moechan599@gmail.com>
2022-04-07 14:02:04 +0630

    session

--- init.php
+++ init.php
@@ -2,8 +2,11 @@
 
 session_start();
 
-define('APP_URL', 'http://localhost:90/blog');
+define('APP_URL', 'http://localhost/blog');
 define('APP_PATH', __DIR__);
 
-include_once APP_PATH . './helper.php';
+include_once APP_PATH . '/helper.php';
 include_once APP_PATH . '/db.php';
+include_once APP_PATH . '/common/Session.php';
+include_once APP_PATH . '/common/Auth.php';
+include_once APP_PATH . '/common/User.php';
